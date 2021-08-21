480606e9b040f38bf0c3a73890c09d718455affb
edevars <e.devars.dev@gmail.com>
2021-08-21 13:25:05 -0500

    :sparkles: Adding services

--- services/user.js
+++ services/user.js
@@ -6,8 +6,60 @@ class UserService {
   }
 
   async createUser(user) {
-    const newUser = await this.table.create(user);
-    return newUser;
+    try {
+      const newUser = await this.table.create(user);
+      return newUser;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async getUsers() {
+    try {
+      const users = await this.table.findAll();
+      return users;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async getUserByEmail(email) {
+    try {
+      const user = await this.table.findAll({
+        where: {
+          email,
+        },
+      });
+      return user;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async updateUserByEmail(fields, email) {
+    try {
+      const updatedUser = await this.table.update(fields, {
+        where: {
+          email,
+        },
+      });
+      return updatedUser;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async deleteUserByEmail(email) {
+    try {
+      const deletedUser = await this.table.destroy({
+        where: {
+          email,
+        },
+      });
+      return deletedUser;
+    } catch (error) {
+      console.error(error);
+    }
   }
 }
 
