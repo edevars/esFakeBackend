a656c6c9fd3b0183dc12d6f25d104e9d629adf7c
edevars <e.devars.dev@gmail.com>
2021-08-21 23:36:30 -0500

    :sparkles: Creating library for cloudinary

--- config/index.js
+++ config/index.js
@@ -9,6 +9,9 @@ const config = {
   dbHost: process.env.DB_HOST,
   dbName: process.env.DB_NAME,
   dev: process.env.NODE_ENV === "development",
+  cloudinaryCloudName: process.env.CLOUDINARY_CLOUD_NAME,
+  cloudinaryApiKey: process.env.CLOUDINARY_API_KEY,
+  cloudinaryApiSecret: process.env.CLOUDINARY_API_SECRET,
 };
 
 module.exports = { config };
